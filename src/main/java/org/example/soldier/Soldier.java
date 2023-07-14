ca15b234cb5e28e489d807d8a5f7644f8c40ce36
Jumelis Rosales <jumelisrosales@gmail.com>
2023-07-14 12:32:58 +0200

    vikings 2.0

--- src/main/java/org/example/soldier/Soldier.java
+++ src/main/java/org/example/soldier/Soldier.java
@@ -1,4 +1,18 @@
 package org.example.soldier;
 
 public class Soldier {
+    public int health;
+    public int strength;
+
+    public Soldier(int health, int strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+    public int attack() {
+        return this.strength;
+    }
+    public int receiveDamage(int damage){
+        this.health = this.health - damage;
+        return this.health;
+    }
 }
--- src/main/java/org/example/vikins/Vikings.java
+++ src/main/java/org/example/vikins/Vikings.java
@@ -1,4 +1,26 @@
-package org.example.vikins;
-
-public class Vikings {
+package org.example.vikins;import org.example.soldier.Soldier;class Viking extends Soldier {
+    public String name;
+    public String msg;
+    public Viking(String name, int health, int strength) {
+        super(health, strength);
+        this.name = name;
+    }
+    public int receiveDamage(int damage) {
+        this.health -= damage;
+        if (this.health > 0) {
+            this.msg = this.name + "has received" + damage + "points of damage";
+        } else {
+            this.msg = this.name + "has died in combat";
+        }
+        return this.health;
+    }
+    public String getMsg() {
+        return this.msg;
+    }
+    public String battleCry(){
+        return "Odin Owns You All!";
+    }
 }
+
+
+
